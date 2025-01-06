00abfaabfcbbcd453a30400d3782fc415ff449ea
Georgian Nechifor <nechiforgeorgian@gmail.com>
2025-01-06 18:07:30 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/macos-react/macos-react",
+  base: "/macos-react/",
   plugins: [vitePluginSvgr(), react()],
   server: {
     port: 3000,
