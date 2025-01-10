170edf0b35fe01c0e17f7c6ef764d1478921192c
Georgian Nechifor <nechiforgeorgian@gmail.com>
2025-01-10 12:48:01 +0200

    fix duplicate settings

--- src/components/macos-footer/MacOSFooter.tsx
+++ src/components/macos-footer/MacOSFooter.tsx
@@ -1,101 +1,95 @@
-import { useRef } from "react";
-import finderIcon from "../../assets/images/webp/finder.png";
-import calculatorIcon from "../../assets/images/webp/calculator.png";
-import vscodeIcon from "../../assets/images/webp/vscode.png";
-import calendarIcon from "../../assets/images/webp/calendar.png";
-import weatherIcon from "../../assets/images/webp/weather.png";
-import photosIcon from "../../assets/images/webp/photos.png";
-import musicIcon from "../../assets/images/webp/applemusic.png";
-import githubIcon from "../../assets/images/webp/github.png";
-import { scaleValue } from "../../utils/scaleValue";
-import "./MacOSFooter.style.css";
+import { useRef } from 'react'
+import finderIcon from '../../assets/images/webp/finder.png'
+import calculatorIcon from '../../assets/images/webp/calculator.png'
+import vscodeIcon from '../../assets/images/webp/vscode.png'
+import calendarIcon from '../../assets/images/webp/calendar.png'
+import weatherIcon from '../../assets/images/webp/weather.png'
+import photosIcon from '../../assets/images/webp/photos.png'
+import musicIcon from '../../assets/images/webp/applemusic.png'
+import githubIcon from '../../assets/images/webp/github.png'
+import { scaleValue } from '../../utils/scaleValue'
+import './MacOSFooter.style.css'
 
-const maxAdditionalSize = 5;
+const maxAdditionalSize = 5
 
 const MacOSFooter = () => {
-  const dockRef = useRef<HTMLDivElement>(null);
+  const dockItems = [
+    {
+      image: finderIcon,
+      title: 'Finder'
+    },
+    {
+      image: photosIcon,
+      title: 'Photos'
+    },
+    {
+      image: calculatorIcon,
+      title: 'Calculator'
+    },
+    {
+      image: vscodeIcon,
+      title: 'VS Code'
+    },
+    {
+      image: calendarIcon,
+      title: 'Calendar'
+    },
+    {
+      image: githubIcon,
+      title: 'GitHub'
+    },
+    {
+      image: weatherIcon,
+      title: 'Weather'
+    },
+    {
+      image: musicIcon,
+      title: 'Apple Music'
+    }
+  ]
+  const dockRef = useRef<HTMLDivElement>(null)
 
   const handleAppHover = (ev: React.MouseEvent<HTMLLIElement>) => {
-    if (!dockRef.current) return;
+    if (!dockRef.current) return
 
-    const mousePosition = ev.clientX;
-    const iconPositionLeft = ev.currentTarget.getBoundingClientRect().left;
-    const iconWidth = ev.currentTarget.getBoundingClientRect().width;
+    const mousePosition = ev.clientX
+    const iconPositionLeft = ev.currentTarget.getBoundingClientRect().left
+    const iconWidth = ev.currentTarget.getBoundingClientRect().width
 
-    const cursorDistance = (mousePosition - iconPositionLeft) / iconWidth;
+    const cursorDistance = (mousePosition - iconPositionLeft) / iconWidth
     const offsetPixels = scaleValue(
       cursorDistance,
       [0, 1],
-      [maxAdditionalSize * -1, maxAdditionalSize],
-    );
+      [maxAdditionalSize * -1, maxAdditionalSize]
+    )
 
     dockRef.current.style.setProperty(
-      "--dock-offset-left",
-      `${offsetPixels * -1}px`,
-    );
+      '--dock-offset-left',
+      `${offsetPixels * -1}px`
+    )
 
     dockRef.current.style.setProperty(
-      "--dock-offset-right",
-      `${offsetPixels}px`,
-    );
-  };
+      '--dock-offset-right',
+      `${offsetPixels}px`
+    )
+  }
 
   return (
     <div className="footer-container">
       <nav ref={dockRef} className="dock">
         <ul>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={finderIcon} />
-              <span className="tooltip">Finder</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={photosIcon} />
-              <span className="tooltip">Photos</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={calculatorIcon} />
-              <span className="tooltip">Calculator</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={vscodeIcon} />
-              <span className="tooltip">VS Code</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={calendarIcon} />
-              <span className="tooltip">Calendar</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={githubIcon} />
-              <span className="tooltip">GitHub</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={weatherIcon} />
-              <span className="tooltip">Weather</span>
-            </button>
-          </li>
-          <li className="dock-item" onMouseMove={handleAppHover}>
-            <button>
-              <img src={musicIcon} />
-              <span className="tooltip">Apple Music</span>
-            </button>
-          </li>
+          {dockItems.map((item, index) => (
+            <li key={index} className="dock-item" onMouseMove={handleAppHover}>
+              <button>
+                <img src={item.image} />
+                <span className="tooltip">{item.title}</span>
+              </button>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default MacOSFooter;
+export default MacOSFooter
--- src/components/settings-dropdown/SettingsDropdown.tsx
+++ src/components/settings-dropdown/SettingsDropdown.tsx
@@ -1,17 +1,115 @@
-import { useEffect, useRef, useState } from 'react'
+import {
+  useEffect,
+  useRef,
+  useState,
+  FC,
+  FunctionComponent,
+  SVGProps
+} from 'react'
 import SettingsIcon from '../../assets/images/svg/settings.svg?react'
 import LocationIcon from '../../assets/images/svg/location.svg?react'
 import AnimationsIcon from '../../assets/images/svg/animations.svg?react'
 import AirdropIcon from '../../assets/images/svg/airdrop.svg?react'
+import BluetoothIcon from '../../assets/images/svg/bluetooth.svg?react'
+import ScreenMirroringIcon from '../../assets/images/svg/screen-mirroring.svg?react'
 
-const SettingsDropdown = () => {
-  const wrapperRef = useRef<HTMLDivElement>(null)
+interface RenderSettingsItemProps {
+  handleOnClick: () => void
+  title: string
+  Icon: FunctionComponent<SVGProps<SVGSVGElement>>
+  isActive: boolean
+  fill?: boolean
+}
 
-  const [settingsOpen, setSettingsOpen] = useState<boolean>(false)
+const SettingsDropdownComponent: FC<{ settingsOpen: boolean }> = ({
+  settingsOpen
+}) => {
   const [locationActive, setLocationActive] = useState<boolean>(false)
   const [airdropActive, setAirdropActive] = useState<boolean>(false)
   const [animationsActive, setAnimationsActive] = useState<boolean>(false)
+  const [screenMirroringActive, setScreenMirroringActive] =
+    useState<boolean>(false)
+  const [bluetoothActive, setBluetoothActive] = useState<boolean>(false)
 
+  const renderSettingsItem = ({
+    handleOnClick,
+    Icon,
+    title,
+    isActive,
+    fill = true
+  }: RenderSettingsItemProps) => (
+    <div className="flex flex-row gap-2 items-center">
+      <div
+        onClick={handleOnClick}
+        className={`${isActive ? 'bg-blue-500' : 'bg-gray-600'} rounded-full w-8 h-8 flex items-center justify-center`}
+      >
+        <Icon height={16} {...(fill ? { fill: '#fff' } : {})} />
+      </div>
+      <div className="flex flex-col">
+        <p className="!text-[12px]">{title}</p>
+        <span className="text-[11px] text-gray-300">
+          {isActive ? 'On' : 'Off'}
+        </span>
+      </div>
+    </div>
+  )
+
+  return (
+    <div
+      className={`absolute top-10 right-2 p-3 min-w-[300px] rounded-xl bg-black/30 ${settingsOpen ? 'opacity-1' : 'opacity-0'} transition-all ease-in-out duration-300`}
+    >
+      <div className="flex flex-row gap-2">
+        <div className="h-fit flex flex-col justify-center gap-4 flex-1 bg-gray-600/20 rounded-xl p-2 border border-1 border-solid border-white/10">
+          {renderSettingsItem({
+            handleOnClick: () => setLocationActive(!locationActive),
+            title: 'Location',
+            Icon: LocationIcon,
+            isActive: locationActive
+          })}
+          {renderSettingsItem({
+            handleOnClick: () => setAirdropActive(!airdropActive),
+            title: 'AirDrop',
+            Icon: AirdropIcon,
+            isActive: airdropActive
+          })}
+          {renderSettingsItem({
+            handleOnClick: () => setBluetoothActive(!bluetoothActive),
+            title: 'Bluetooth',
+            Icon: BluetoothIcon,
+            isActive: bluetoothActive
+          })}
+        </div>
+
+        <div className="flex flex-col gap-2 flex-1">
+          <div className="h-fit flex flex-col justify-center gap-4 flex-1 bg-gray-600/20 rounded-xl p-2 border border-1 border-solid border-white/10">
+            {renderSettingsItem({
+              handleOnClick: () => setAnimationsActive(!animationsActive),
+              title: 'Animations',
+              Icon: AnimationsIcon,
+              isActive: animationsActive
+            })}
+          </div>
+          <div className="h-fit flex flex-col justify-center gap-4 flex-1 bg-gray-600/20 rounded-xl p-2 border border-1 border-solid border-white/10">
+            {renderSettingsItem({
+              handleOnClick: () =>
+                setScreenMirroringActive(!screenMirroringActive),
+              title: 'Screen Mirroring',
+              Icon: ScreenMirroringIcon,
+              fill: false,
+              isActive: screenMirroringActive
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const SettingsDropdown = () => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [settingsOpen, setSettingsOpen] = useState<boolean>(false)
+
+  // close settings dropdown when clicking outside its container
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       // @ts-expect-error current is node and event target is eventTarge type
@@ -36,78 +134,7 @@ const SettingsDropdown = () => {
         onClick={() => setSettingsOpen(!settingsOpen)}
       />
 
-      <div
-        className={`absolute top-10 right-2 p-3 min-w-[300px] rounded-xl bg-black/30 ${settingsOpen ? 'opacity-1' : 'opacity-0'} transition-all ease-in-out duration-300`}
-      >
-        <div className="flex flex-row gap-2">
-          <div className="h-fit flex flex-col gap-4 flex-1 bg-gray-600/20 rounded-xl p-2 border border-1 border-solid border-white/10">
-            <div className="flex flex-row gap-2 items-center">
-              <div
-                onClick={() => setLocationActive(!locationActive)}
-                className={`${locationActive ? 'bg-blue-500' : 'bg-gray-600'} rounded-full w-8 h-8 flex items-center justify-center`}
-              >
-                <LocationIcon height={16} fill="#fff" />
-              </div>
-              <div className="flex flex-col">
-                <p className="!text-[12px]">Location</p>
-                <span className="text-[11px] text-gray-300">
-                  {locationActive ? 'On' : 'Off'}
-                </span>
-              </div>
-            </div>
-
-            <div className="flex flex-row gap-2 items-center">
-              <div
-                onClick={() => setAirdropActive(!airdropActive)}
-                className={`${airdropActive ? 'bg-blue-500' : 'bg-gray-600'} rounded-full w-8 h-8 flex items-center justify-center`}
-              >
-                <AirdropIcon height={16} fill="#fff" />
-              </div>
-              <div className="flex flex-col">
-                <p className="!text-[12px]">AirDrop</p>
-                <span className="text-[11px] text-gray-300">
-                  {airdropActive ? 'On' : 'Off'}
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-2 flex-1">
-            <div className="h-fit flex flex-col gap-3 flex-1 bg-gray-600/20 rounded-xl p-1 items-center justify-center border border-1 border-solid border-white/10">
-              <div className="flex flex-row gap-2 items-center">
-                <div
-                  onClick={() => setAnimationsActive(!animationsActive)}
-                  className={`${animationsActive ? 'bg-blue-500' : 'bg-gray-600'} rounded-full w-7 h-7 flex items-center justify-center`}
-                >
-                  <AnimationsIcon height={15} fill="white" />
-                </div>
-                <div className="flex flex-col">
-                  <p className="!text-[12px]">Animations</p>
-                  <span className="text-[11px] text-gray-300">
-                    {animationsActive ? 'On' : 'Off'}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="h-fit flex flex-col gap-3 flex-1 bg-gray-600/20 rounded-xl p-1 items-center justify-center border border-1 border-solid border-white/10">
-              <div className="flex flex-row gap-2 items-center">
-                <div
-                  onClick={() => setAnimationsActive(!animationsActive)}
-                  className={`${animationsActive ? 'bg-blue-500' : 'bg-gray-600'} rounded-full w-7 h-7 flex items-center justify-center`}
-                >
-                  <AnimationsIcon height={15} fill="white" />
-                </div>
-                <div className="flex flex-col">
-                  <p className="!text-[12px]">Animations</p>
-                  <span className="text-[11px] text-gray-300">
-                    {animationsActive ? 'On' : 'Off'}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SettingsDropdownComponent settingsOpen={settingsOpen} />
     </div>
   )
 }
